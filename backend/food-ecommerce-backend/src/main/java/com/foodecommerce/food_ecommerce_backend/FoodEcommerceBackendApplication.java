5d947635941d059ba5e32638bb1dc98e34617c7e
caoquocdung <caoquocdung@protonmail.com>
2025-06-18 19:28:06 +0700

    backend initialized

---
+++ backend/food-ecommerce-backend/src/main/java/com/foodecommerce/food_ecommerce_backend/FoodEcommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodecommerce.food_ecommerce_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodEcommerceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodEcommerceBackendApplication.class, args);
+	}
+
+}
