c7619dcc2bb61d713077c7b820e1e40360ead28d
caoquocdung <caoquocdung@protonmail.com>
2025-06-19 17:28:51 +0700

    food api created

--- backend/init_db.py
+++ backend/init_db.py
@@ -2,11 +2,13 @@ import asyncio
 from core.db import engine
 from models.user import Base as UserBase
 from models.category import Base as CategoryBase
+from models.food import Base as FoodBase
 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(UserBase.metadata.create_all)
         await conn.run_sync(CategoryBase.metadata.create_all)
+        await conn.run_sync(FoodBase.metadata.create_all)
 
 if __name__ == "__main__":
     asyncio.run(init_db())
--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 # main.py
 from fastapi import FastAPI
-from routes import auth, category
+from routes import auth, category, food
 
 app = FastAPI(title="Authentication API")
 
 app.include_router(auth.router, prefix="/api/auth", tags=["Auth"])
 app.include_router(category.router, prefix="/api", tags=["Category"])
+app.include_router(food.router, prefix="/api", tags=["Food"])
 
 @app.get("/")
 def read_root():
