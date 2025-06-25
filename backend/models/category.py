a6717881137c1a38d5a88d47dcecaa4ee48e8c1f
caoquocdung <caoquocdung@protonmail.com>
2025-06-25 18:30:55 +0700

    xoa het tat ca

--- backend/models/category.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from core.db import Base
-
-class Category(Base):
-    __tablename__ = "categories"
-    category_id = Column(Integer, primary_key=True, index=True)
-    category_name = Column(String, unique=True, nullable=False)
-    category_icon_url = Column(String, nullable=True)
